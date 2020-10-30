2a4dbe7826b7fcd08dbd385c7cb805c6b1f0627d
vorsurm <moshiur1412@gmail.com>
2020-10-30 22:02:55 +0600

    PHP Variable Practice

---
+++ PHP_Basic/04_PhpVariable.php
@@ -0,0 +1,39 @@
+<?php
+	# 
+$A = "A";
+$a = "a";
+$_a = "_a";
+$a1 = "a1";
+$a_b = "a_b";
+$aB = "aB";
+
+$number1 =5;
+$number2 =5.2;
+
+echo $A;
+echo "<br>";
+echo $a;
+
+var_dump($number1);
+
+
+
+# PHP Predefined Variables
+1. $GLOBALS
+2. $_SERVER
+3. $_GET
+4. $_POST
+5. $_FILES
+6. $_COOKIES
+7. $_SESSION
+8. $_REQUEST
+9. $_ENV
+
+
+# User Defined Variables:
+1. Variable Scope
+2. Variable Variables
+3. Reference Variable
+
+
+?>
