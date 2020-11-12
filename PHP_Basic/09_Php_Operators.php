fe1eaebffcadc91e7a1c1bc9843bef26d65b364c
vorsurm <moshiur1412@gmail.com>
2020-11-12 10:01:20 +0600

    PHP Operations Kinds

---
+++ PHP_Basic/09_Php_Operators.php
@@ -0,0 +1,20 @@
+<?php
+	# PHP Operators:: It's a programming languare symbol which is interpreter assist methematical, relational and logical operation.
+
+# PHP Operations are 11 kinds of Operations:
+
+// 1. Arithmetic Operators (mathmatic): (+-*/**%)
+// 2. Assignment Operators (kept variable value): =
+// 3. Bitwise Operators (Binary worked): -(&|^~<<>>)
+// 4. Comparison Operators (Compare two values): == != === !== < > <= >= <=>
+// 5. Error Control Operators: @
+// 6. Execution Operators (shell command) : `
+// 7. Incrementing/Decrementing Operators (+1, -1): ++ --
+// 8. Logical Operators: &&, ||, ! and or xor
+// 9. String Operators: .
+// 10. Array Operators (compare, union for array): +, ==, !=, ===, !==, <>
+// 11. Type Operators(check class instance): instanceof
+
+
+
+?>
