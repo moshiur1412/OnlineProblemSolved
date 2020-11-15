de4a4383eea8e3c603c0a0c3bc783683b9c35ad0
vorsurm <moshiur1412@gmail.com>
2020-11-15 11:22:23 +0600

    PHP Arithmetic Operation

---
+++ PHP_Basic/10_PHP_Arithmetic_Assignment_Comparison_and_String_Operators.php
@@ -0,0 +1,15 @@
+<?php
+	echo 8+5; // 13
+	echo "<br>";
+	echo 8-5; // 3
+	echo "<br>";
+	echo 8*5; // 40
+	echo "<br>";
+	echo 8/5; // 1.6
+	echo "<br>";
+	echo 8%5; // 3
+	echo "<br>";
+	echo 8**5; // 32768
+	echo "<br>";
+
+?>
