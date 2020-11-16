bbb1c46e6c0f474856bf4b38dd21074c00e648d3
vorsurm <moshiur1412@gmail.com>
2020-11-16 10:38:50 +0600

    Pre & Post Increment Testing 1

---
+++ PHP_Basic/11_PHP_Increment_Decrement_Operators.php
@@ -0,0 +1,27 @@
+<?php
+$c = 10;
+$c++;
+echo $c; // 11
+echo "<br>";
+$c--;
+echo $c; // 10
+
+echo "<br>";
+
+/**
+* Increment Operator Type
+* Pre Increment: First increment then print
+* Post Increment:
+*/
+
+$c = 100;
+$c++; // Pre Increment;
+echo $c;
+echo "<br>". $c++ . "<br>"; // Post Increment;
+echo $c
+
+
+
+
+
+?>
