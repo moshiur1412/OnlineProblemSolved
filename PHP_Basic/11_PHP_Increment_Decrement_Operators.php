c91fd8af7d44eb04b7945b86a0c711466aa38a61
vorsurm <moshiur1412@gmail.com>
2020-11-16 10:45:01 +0600

    Increment & Decrement Method done

--- PHP_Basic/11_PHP_Increment_Decrement_Operators.php
+++ PHP_Basic/11_PHP_Increment_Decrement_Operators.php
@@ -9,19 +9,29 @@ echo $c; // 10
 echo "<br>";
 
 /**
-* Increment Operator Type
-* Pre Increment: First increment then print
-* Post Increment:
+* Increment/Decriment Operator Type
+* Pre Increment: First increment/Decriment then print
+* Post Increment: First print then Increment/Decriment
 */
 
 $c = 100;
 $c++; // Pre Increment;
-echo $c;
-echo "<br>". $c++ . "<br>"; // Post Increment;
-echo $c
+echo $c; // 101
+echo "<br>". $c++ . "<br>"; // 101 Post Increment;
+echo $c; // 102
 
 
+// pre incrent antother way
+++$c;
+echo "<br>".$c; // 103
 
 
+# Pre Decrement
+--$c;
+echo "<br>".$c; // 102
+
+# Post Decrement
+$c--;
+echo "<br>".$c; // 101
 
 ?>
