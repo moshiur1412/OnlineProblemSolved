a555543e8f926c28329df83f9002087fd1788fed
vorsurm <moshiur1412@gmail.com>
2020-11-19 09:49:09 +0600

    PHP Null Coalescing Operator

--- PHP_Basic/13_PHP_Ternary_and_Null_Coalescing_Operator.php
+++ PHP_Basic/13_PHP_Ternary_and_Null_Coalescing_Operator.php
@@ -6,4 +6,17 @@
 
 	echo $age<30?"You'r eligible":"Not eligible";
 
+	echo $age <=30 ?? "Default"; // 1
+
+
+   # Null Coalescing Operator
+	$foo = null;
+	$bar = null;
+	$baz = 1;
+	$quz =2;
+
+	echo $foo??$quz; //2
+
+	echo $foo??$bar??$baz??$quz; // 1
+
 ?>
