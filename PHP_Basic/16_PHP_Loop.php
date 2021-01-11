95b3be60c80a3369830a6f684deb135816de3adf
vorsurm <moshiur1412@gmail.com>
2021-01-11 17:17:06 +0600

    PHP Loop with sequence number

--- PHP_Basic/16_PHP_Loop.php
+++ PHP_Basic/16_PHP_Loop.php
@@ -1,20 +1,44 @@
 <?php 
-	$i = 1;
 
-	// for($i=0; $i<10; $i++){
-	// 	 echo $i;
+	// # Regular used --->
+	// for($i=1; $i <= 10; $i++){ // start; condition; increment
+	// 	echo $i, "<br>";
 	// }
 
-	// while($i <= 100){ // condition is true
-	// 	echo $i."<br>";
-	// 	$i+=10;
+	// # Changed increment -->
+	// for($i=1; $i <11; $i+=2):
+	// 	echo $i, "<br>";
+	// endfor;
+
+
+
+	// # used body for condition
+	// $i =1;
+	// for(;;){
+	// 	if($i<11){
+	// 		echo $i, "<br>";
+	// 		$i++;
+	// 	}else {
+	// 		break;
+	// 	}
 	// }
 
 
-	do{
-		echo "for do $i"."<br>";
-		$i++;
-	}while($i <=9 )
+	# Sequence Number -->
+	// 0 1 1 2 3 5 8 13 
+	
+	$f1 = 0;
+	$f2 = 1;
+	$f = 1;
+	$f3 = 2;
+
+	echo $f1, "<br>", $f2, "<br>"; // 0 1 
+	for($i=0; $i<6; $i++){
+		$f3 = $f2+$f1; // 1+0
+		$f1 = $f2;
+		$f2 = $f3;
+		echo $f3, "<br>"; // 1 2 3 4 8 13 
+	}
 
 
  ?>
