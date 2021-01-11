371b1944399016bdf918967677a6d238e735ef87
vorsurm <moshiur1412@gmail.com>
2021-01-11 16:45:39 +0600

    PHP Loop Variation part -1

---
+++ PHP_Basic/16_PHP_Loop.php
@@ -0,0 +1,20 @@
+<?php 
+	$i = 1;
+
+	// for($i=0; $i<10; $i++){
+	// 	 echo $i;
+	// }
+
+	// while($i <= 100){ // condition is true
+	// 	echo $i."<br>";
+	// 	$i+=10;
+	// }
+
+
+	do{
+		echo "for do $i"."<br>";
+		$i++;
+	}while($i <=9 )
+
+
+ ?>
