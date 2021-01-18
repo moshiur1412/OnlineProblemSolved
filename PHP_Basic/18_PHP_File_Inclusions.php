a08b14a9ab3e91a2abebac4a74fc22e7c804633b
vorsurm <moshiur1412@gmail.com>
2021-01-18 17:17:52 +0600

    Refactoring the file incusion factors

--- PHP_Basic/18_PHP_File_Inclusions.php
+++ PHP_Basic/18_PHP_File_Inclusions.php
@@ -1,5 +1,5 @@
 <?php
- # Four Types
+ # Four Types of php file inclusion
  # 1. include
  # 2. include_once
  # 3. require
@@ -8,7 +8,10 @@
  include('16_PHP_Loop.php');
  include_once('16_PHP_Loop.php');
 
- require('17_PHP_Loop.php'); // If file missing that wouldn't execute next line 
+ # Still error with comment section
+//  require('17_PHP_Loop.php'); // If file missing that wouldn't execute next line 
  echo "This is not worked for file not found";
  require_once('17_PHP_Loop.php');
+ echo "don't read this line";
+
 ?>
