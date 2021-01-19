9a8d6065049cce5a15c54f7034da88c2fb533cfc
vorsurm <moshiur1412@gmail.com>
2021-01-19 15:43:58 +0600

    PHP Arugument and handling functions

--- PHP_Basic/19_PHP_Functions_Parameters_and_Arguments.php
+++ PHP_Basic/19_PHP_Functions_Parameters_and_Arguments.php
@@ -10,10 +10,29 @@
 
 # User defined functions
 function basicFunction(){
-    echo "This is the basic function";
+    echo "This is the basic function", "<hr>";
 }
 basicFunction();
 
 # Function arguments
+
+function argumentsFunction(...$params){
+    
+    # Return an item from the argument list;
+    print_r(func_get_arg(1));
+   
+    # Returns an array comprising a functions argument list
+    echo "<pre>"; print_r(func_get_args($params));
+    // Array ( [0] => 1 [1] => 2 [2] => 3 [3] => 4 )
+   
+    # Returns the number of arguments passed to the function
+    echo func_num_args(), "<br>";
+    // 4
+    print_r($params);
+    // Array ( [0] => 1 [1] => 2 [2] => 3 [3] => 4 )
+
+    echo "<hr>";
+}
+argumentsFunction(1,2,3,4);
 
 
