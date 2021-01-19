b5625d9b5b0a2387c21493f8975d1f30c7c0fdde
vorsurm <moshiur1412@gmail.com>
2021-01-19 15:30:07 +0600

    PHP Functions

---
+++ PHP_Basic/19_PHP_Functions_Parameters_and_Arguments.php
@@ -0,0 +1,19 @@
+<?php
+# All Functions
+# 1. User-defined Functions
+# 2. Function Arguments
+# 3. Returing Values
+# 4. Variable Functions
+# 5. Internal (Build-in) Functions
+# 6. Anonymous Functions
+# 7. Arrow Functions
+
+# User defined functions
+function basicFunction(){
+    echo "This is the basic function";
+}
+basicFunction();
+
+# Function arguments
+
+
