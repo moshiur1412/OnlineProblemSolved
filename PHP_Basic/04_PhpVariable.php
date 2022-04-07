94c178a1848e1f50c6022cc9dc58f207d3cc81d6
vorsurm <moshiur1412@gmail.com>
2022-04-08 00:00:06 +0600

    PHP getype() and settype() checked

--- PHP_Basic/04_PhpVariable.php
+++ PHP_Basic/04_PhpVariable.php
@@ -13,27 +13,41 @@ $number2 =5.2;
 echo $A;
 echo "<br>";
 echo $a;
+echo "<br>";
 
-var_dump($number1);
+var_dump($number1); // int(5)
+
+echo "<br>";
+
+echo gettype($number1); // integer
+
+echo "<br>";
+
+settype($a, "integer");
+
+echo "<br>";
+
+var_dump($a); // int(0)
+
 
 
 
 # PHP Predefined Variables
-1. $GLOBALS
-2. $_SERVER
-3. $_GET
-4. $_POST
-5. $_FILES
-6. $_COOKIES
-7. $_SESSION
-8. $_REQUEST
-9. $_ENV
+// 1. $GLOBALS
+// 2. $_SERVER
+// 3. $_GET
+// 4. $_POST
+// 5. $_FILES
+// 6. $_COOKIES
+// 7. $_SESSION
+// 8. $_REQUEST
+// 9. $_ENV
 
 
 # User Defined Variables:
-1. Variable Scope
-2. Variable Variables
-3. Reference Variable
+// 1. Variable Scope
+// 2. Variable Variables
+// 3. Reference Variable
 
 
 ?>
